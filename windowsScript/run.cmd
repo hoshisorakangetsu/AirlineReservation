9e2a19b22478e165b11064416799cd106ab66e16
hoshisorakangetsu <87136376+hoshisorakangetsu@users.noreply.github.com>
2023-04-06 15:46:41 +0800

    Update windows run script

--- windowsScript/run.cmd
+++ windowsScript/run.cmd
@@ -1,2 +1,2 @@
-set JAVA_HOME=".\.dev-kits\jdk-18.0.1.1"
+set JAVA_HOME=.\.dev-kits\jdk-18.0.1.1
 "%JAVA_HOME%\bin\java.exe" -cp ".\target\classes" com.mycompany.airlinereservation.AirlineReservation
