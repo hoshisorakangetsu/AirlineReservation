07e44f5012d2a42aab8ac70bdbc757fa8a91ebb7
YhChiam <chiamyh-pp21@student.tarc.edu.my>
2023-04-13 12:50:05 +0800

    Test changes push

--- src/main/java/com/mycompany/airlinereservation/AirlineReservation.java
+++ src/main/java/com/mycompany/airlinereservation/AirlineReservation.java
@@ -1,8 +1,12 @@
 package com.mycompany.airlinereservation;
 
+import util.*;
+
 public class AirlineReservation {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        String s = ConsoleInput.getString("Test, string please?: ");
+        System.out.println(s);
     }
 }
