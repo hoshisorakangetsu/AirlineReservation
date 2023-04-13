041cbc121e2a9d4a73721f2453f4cf8b7346da67
hoshisorakangetsu <87136376+hoshisorakangetsu@users.noreply.github.com>
2023-04-13 11:55:26 +0800

    Add utility console input functions

---
+++ src/main/java/com/mycompany/airlinereservation/util/ConsoleInput.java
@@ -0,0 +1,47 @@
+package util;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+// utility class to help get input faster with code (write one line instead of 2)
+// handles exception as well
+public class ConsoleInput {
+  private static Scanner scanner = new Scanner(System.in);
+
+  // won't appear InputMismatchException, other Exception is unexpected, so cannot handle
+  public static String getString(String prompt) {
+    return scanner.nextLine();
+  }
+
+  public static int getInt(String prompt) {
+    while (true) {
+      try {
+        System.out.print(prompt);
+        return scanner.nextInt();
+      } catch (InputMismatchException e) {
+        System.out.println("Invalid input: Please enter a valid integer");
+        scanner.nextLine();  // consume invalid token
+      }
+    }
+  }
+
+  public static double getDouble(String prompt) {
+    while (true) {
+      try {
+        System.out.print(prompt);
+        return scanner.nextDouble();
+      } catch (InputMismatchException e) {
+        System.out.println("Invalid input: Please enter a valid decimal");
+        scanner.nextLine();  // consume invalid token
+      }
+    }
+  }
+
+  public static char getChar(String prompt) {
+    String res = scanner.nextLine();
+    if (res.length() > 1) {
+      System.out.println("Input has more than one character, ignoring the rest...");
+    }
+    return res.charAt(0);
+  }
+}
