b3dc3398aa6c5c8ef7976189f515a45cf7a8a250
yongyk <yikityong@gmail.com>
2023-05-02 14:52:21 +0800

    update reservation.java

--- src/main/java/com/mycompany/airlinereservation/entity_classes/Reservation.java
+++ src/main/java/com/mycompany/airlinereservation/entity_classes/Reservation.java
@@ -9,5 +9,49 @@ package com.mycompany.airlinereservation.entity_classes;
  * @author yong
  */
 public class Reservation {
-    
+    private PlaneTicket[] tickets;
+    private Payment pay;
+    private Customer cust;
+    public Reservation (){
+        
+    }
+    public Reservation(PlaneTicket[] tickets, Payment pay,Customer cust){
+        this.tickets=tickets;
+        this.pay=pay;
+        this.cust=cust;
+    }
+    public PlaneTicket[] getPlaneTicket(){
+        return tickets;
+    }
+    public Payment getPay(){
+        return pay;
+        
+    }
+    public Customer getCust(){
+        return cust;
+        
+    }
+    public void setTicket(PlaneTicket[] tickets){
+        this.tickets=tickets;
+    }
+    public void setPay(Payment pay){
+        this.pay=pay;
+    }
+    public void setCust(Customer cust){
+        this.cust=cust;
+    }
+   public String toString() {
+    String result = "";
+    for (String ticket : tickets) {
+        result += String.format("Ticket: %s \n Payment : %.2f \n Customer: %s\n", ticket, pay, cust);
+    }
+    return result;
+    }
+   public boolean equals(Object o){
+       if (o instanceof Reservation ){
+           return (cust.equals(((Reservation)o).cust));
+       }else {
+           return false;
+       }
+   }
 }
