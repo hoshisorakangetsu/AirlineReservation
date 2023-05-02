8484dbb2e595648f4a215ab17960e095be425994
yongyk <yikityong@gmail.com>
2023-05-02 17:14:08 +0800

    update cash.java

--- src/main/java/com/mycompany/airlinereservation/entity_classes/Cash.java
+++ src/main/java/com/mycompany/airlinereservation/entity_classes/Cash.java
@@ -8,6 +8,24 @@ package com.mycompany.airlinereservation.entity_classes;
  *
  * @author yong
  */
-public class Cash {
+
+
+public class Cash extends Payment{
     
+    public Cash (){
+        super();
+    }
+    public Cash (Date paymentDate,int amount){
+        super (paymentDate, amount);
+    }
+    public String toString (){
+        return String.format("Payment: %.2f\n Date Payment: %s", paymentDate, amount);
+    }
+     public boolean equals(Object o){
+       if (o instanceof Cash){
+           return (paymentDate.equals(((Cash)o).paymentDate));
+       }else {
+           return false;
+       }
+   }
 }
